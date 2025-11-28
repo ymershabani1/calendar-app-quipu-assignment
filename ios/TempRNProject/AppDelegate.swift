88ef086ed33b977062959c7af16e10e77204d371
ymershabani1 <ymer.shabani94@gmail.com>
2025-11-28 05:49:17 +0100

    Fix: iOS project configuration

--- ios/TempRNProject/AppDelegate.swift
+++ ios/TempRNProject/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "TempRNProject",
+      withModuleName: "CalendarApp",
       in: window,
       launchOptions: launchOptions
     )
